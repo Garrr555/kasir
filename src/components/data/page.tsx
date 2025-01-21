b53bf9a7b36da364e15ffa6403727826f82fb2ec
Mohammad Tegar Ibrahim <mohammadtegaribrahim@gmail.com>
2025-01-21 21:25:51 +0700

    updatemenu

--- src/components/data/page.tsx
+++ src/components/data/page.tsx
@@ -1,36 +1,51 @@
 export const menu = [
   {
     id: 1,
-    nama: "menu1",
-    harga: 1,
+    nama: "Dada, Paha Atas Besar",
+    harga: 10000,
     imageUrl:
       "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
   },
   {
     id: 2,
-    nama: "menu2",
-    harga: 2,
+    nama: "Dada, Paha Atas Kecil",
+    harga: 8000,
     imageUrl:
       "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp",
   },
   {
     id: 3,
-    nama: "menu3",
-    harga: 3,
+    nama: "Paha Bawah, sayap ",
+    harga: 8000,
     imageUrl:
       "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
   },
   {
     id: 4,
-    nama: "menu4",
-    harga: 4,
+    nama: "SAMBAL GEPREK",
+    harga: 2500,
     imageUrl:
       "https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp",
   },
   {
     id: 5,
-    nama: "menu5",
-    harga: 5,
+    nama: "SATE AYAM",
+    harga: 4000,
+    imageUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+  },
+
+  {
+    id: 6,
+    nama: "KULIT",
+    harga: 7000,
+    imageUrl:
+      "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
+  },
+  {
+    id: 7,
+    nama: "ES TEH",
+    harga: 3000,
     imageUrl:
       "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp",
   },
